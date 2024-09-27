7a926a7fc7f4678667bab73c86e62c176f39338a
wolamey <salamenikava77@gmail.com>
2024-09-27 15:19:03 +0300

    testimonails

--- src/Components/SwiperTestimonails/SwiperTestimonails.jsx
+++ src/Components/SwiperTestimonails/SwiperTestimonails.jsx
@@ -1,50 +1,63 @@
 import React from "react";
 import { useState } from "react";
-import {  EffectCoverflow, Navigation } from "swiper/modules";
+import { EffectCoverflow, Navigation, Autoplay } from "swiper/modules";
+
 import { Swiper, SwiperSlide } from "swiper/react";
 import testimolail_data from "../../data/testimonails.json";
-import arrow from '../../assets/arrow-slider.png'
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import "swiper/css/effect-coverflow";
+import "swiper/css/autoplay";
+
 import "./SwiperTestimonails.scss";
+
 export default function SwiperTestimonails() {
-
   return (
-    <div className="swiper_outer">
-       <p className="title testimonails__title services__title">отзывы наших клиентов</p>
-      <Swiper
-        effect={"slide"}
-        grabCursor={true}
-        centeredSlides={false}
-        slidesPerView={window.innerWidth < 1200 ? 1 : 2}
-        spaceBetween={50}
-        loop={true}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 0,
-          depth: 200,
-          modifier: 0,
-          slideShadows: true,
-        }}
-        navigation={true}
-   
-        modules={[EffectCoverflow,  Navigation]}
-        className="mySwiper"
-      >
-        
-        {testimolail_data.map(({img, name, profession, description}, index) => (
-          <SwiperSlide key={index} className="init_container">
-            <img src={img} className="swiper__img"/>
-            <p className="swiper__name">{name}</p>
-            <p className="swiper__prof">{profession}</p>
-            <p className="swiper__description">{description}</p>
+    <div className="swiper">
+      <p className="title testimonails__title services__title">
+        отзывы наших клиентов
+      </p>
+      <div className="swiper_outer">
+        <div className="swiper__filter-right"></div>
+        <div className="swiper__filter-left"></div>
 
-          </SwiperSlide>
-        ))}
-      </Swiper>
+        <Swiper
+          effect={"slide"}
+          grabCursor={true}
+          centeredSlides={window.innerWidth < 1024 ? true : true}
+          slidesPerView={
+            window.innerWidth < 800
+              ? 1
+              : window.innerWidth < 1024
+              ? 2
+              : window.innerWidth < 1700
+              ? 2
+              : 3
+          }
+          spaceBetween={150}
+          loop={true}
+          speed={5000}
+          autoplay={{
+            delay: -1,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay, EffectCoverflow, Navigation]}
+          className="mySwiper"
+        >
+          {testimolail_data.map(
+            ({ img, name, profession, description }, index) => (
+              <SwiperSlide key={index} className="init_container">
+                <img src={img} className="swiper__img" />
+                <p className="swiper__name">{name}</p>
+                <p className="swiper__prof">{profession}</p>
+                <p className="swiper__description">{description}</p>
+              </SwiperSlide>
+            )
+          )}
+        </Swiper>
+      </div>
     </div>
   );
 }
